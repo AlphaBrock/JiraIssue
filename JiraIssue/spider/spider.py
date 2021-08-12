b49875d465f2a66e42f33a0987caaafe14db866a
AlphaBrock <linjcciam1314@gmail.com>
2021-08-12 17:21:47 +0800

    jira封装接口

--- JiraIssue/spider/spider.py
+++ JiraIssue/spider/spider.py
@@ -7,31 +7,29 @@
    date        :    2021/8/10
 -------------------------------------------------
 """
-import requests
 import json
-import csv
-import time
+
+import requests
 from lxml import etree
-import logging
-from logging import handlers
+
 from JiraIssue.utils.logger import Logger
 
-logger = Logger(loggeNname="spider")
+log = Logger(loggeNname="spider")
 
 
 class Jira(object):
 
-    def __init__(self):
-        self.BaseUrl = "jira.rizhiyi.com:8443"
+    def __init__(self, BaseUrl, JiraUserName, JiraUserPasswd):
+        self.BaseUrl = BaseUrl
+        self.JiraUserName = JiraUserName
+        self.JiraUserPasswd = JiraUserPasswd
         self.Headers = {
             'Host': self.BaseUrl,
             'Origin': 'https://{}'.format(self.BaseUrl),
-            'Cookie': Cookie,
             'X-Atlassian-Token': 'no-check',
-            'X-Requested-With': 'XMLHttpRequest',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
-            # 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
         }
+        self.login()
 
     @staticmethod
     def parseResults(html):
@@ -48,9 +46,9 @@ class Jira(object):
         if kwargs.get("payload"):
             try:
                 response = requests.request(kwargs.get("method"), kwargs.get("url"), headers=kwargs.get("headers"),
-                                            data=kwargs.get("payload"), verify=False, timeout=60)
-                if response.status_code == 200:
-                    return json.loads(response.text)
+                                            data=kwargs.get("payload"), allow_redirects=False, verify=False, timeout=60)
+                if response.status_code == 200 or response.status_code == 302:
+                    return response
                 return None
             except Exception as e:
                 log.logger.exception(json.dumps(kwargs, ensure_ascii=False) + "获取异常:[{}]".format(str(e)))
@@ -60,107 +58,120 @@ class Jira(object):
             response = requests.request(kwargs.get("method"), kwargs.get("url"), headers=kwargs.get("headers"),
                                         verify=False, timeout=60)
             if response.status_code == 200:
-                # return json.loads(response.text)
-                return response.text
+                return response
             return None
         except Exception as e:
             log.logger.exception(json.dumps(kwargs, ensure_ascii=False) + "获取异常:[{}]".format(str(e)))
             return None
 
-    def getIssueLists(self):
+    def login(self):
+        """
+        模拟登陆获取Cookie
+        :return:
+        """
+        url = "https://{}/login.jsp".format(self.BaseUrl)
+        self.Headers["Referer"] = url
+        self.Headers["Content-Type"] = "application/x-www-form-urlencoded"
+
+        payload = {
+            "os_username": self.JiraUserName,
+            "os_password": self.JiraUserPasswd,
+            "os_destination": "",
+            "user_role": "",
+            "atl_token": "",
+            "login": "登陆"
+        }
+        result = self.httpRequests(url=url, method="POST", payload=payload, headers=self.Headers)
+        cookieJsessionId = result.headers["Set-Cookie"].split(";")[0]
+
+        url = "https://{}".format(self.BaseUrl)
+        result = self.httpRequests(url=url, method="GET", headers=self.Headers)
+        cookieToken = result.headers["Set-Cookie"].split(";")[0].replace("lout", "lin")
+        self.Headers["Cookie"] = cookieJsessionId+";"+cookieToken
+
+    def getIssueLists(self, jql: str, filterId: str):
+        """
+        按照条件筛选Issue
+        :return:
+        """
         url = "https://{}/rest/issueNav/1/issueTable".format(self.BaseUrl)
-        self.Headers["Cookie"] = Cookie
         self.Headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
         self.Headers["Referer"] = "https://{}/issues/?jql=project%20%3D%20RZY".format(self.BaseUrl)
         payload = {
             "startIndex": 0,
-            "jql": 'project = RZY AND status in (Open, "In Progress", Reopened, "To Do", "In Review") order by priority DESC,updated DESC',
-            "filterId": "-5",
+            # "jql": 'project = RZY AND status in (Open, "In Progress", Reopened, "To Do", "In Review") order by priority DESC,updated DESC',
+            "jql": jql,
+            "filterId": filterId,
             "layoutKey": "split-view"
         }
-        result = self.httpRequests(url=url, method="POST", payload=payload, headers=self.Headers)
+
+        result = json.loads(self.httpRequests(url=url, method="POST", payload=payload, headers=self.Headers).text)
         if result:
             issueKeys = result["issueTable"]["issueKeys"]
             return issueKeys
 
-    def getIssueHtmlData(self):
-        # issueKeys = self.getIssueLists()
-        # del self.Headers["Referer"]
-        # del self.Headers["Content-Type"]
-        self.Headers["Cookie"] = Cookie
-        with open("jiraIssueData.csv", "a+", encoding='utf-8') as csv_file:
-            fieldnames = ["URL", "标题", "类型", "状态", "优先级", "解决结果", "影响版本", "修复的版本", "模块", "标签", "描述", "经办人", "报告人",
-                          "创建时间"]
-            csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames, quoting=csv.QUOTE_ALL, escapechar='\\',
-                                        quotechar='"')
-            csv_writer.writeheader()
-            # for issueKey in issueKeys:
-            for issueKey in range(1, 7500):
-                url = "https://{}/browse/RZY-{}".format(self.BaseUrl, issueKey)
-                log.logger.debug("issueKey:{}".format(url))
-                try:
-                    result = self.httpRequests(url=url, method="GET", headers=self.Headers)
-                    parseHtml = self.parseResults(result)
+    def getIssueHtmlData(self, issueKey: str) -> dict:
+        """
+        获取单个Issue详情
+        :return:
+        """
+        url = "https://{}/browse/RZY-{}".format(self.BaseUrl, issueKey)
+        log.logger.debug("issueKey:{}".format(url))
+        result = self.httpRequests(url=url, method="GET", headers=self.Headers)
+        parseHtml = self.parseResults(result)
 
-                    issueTitle = parseHtml.xpath(
-                        "//header[contains(@class, 'aui-page-header')]/div[contains(@class, 'aui-page-header-inner')]/div[contains(@class, 'aui-page-header-main')]/h1/text()")[
-                        0]
+        issueTitle = parseHtml.xpath(
+            "//header[contains(@class, 'aui-page-header')]/div[contains(@class, 'aui-page-header-inner')]/div[contains(@class, 'aui-page-header-main')]/h1/text()")[
+            0]
 
-                    # issue详情
-                    issueType = "".join(parseHtml.xpath('//*[@id="type-val"]/text()')).replace("\n", "").replace(" ", "")
-                    issueStatus = "".join(parseHtml.xpath('//*[@id="status-val"]/span/text()')).replace("\n", "").replace(
-                        " ", "")
-                    issuePriority = "".join(parseHtml.xpath('//*[@id="priority-val"]/text()')).replace("\n", "").replace(
-                        " ", "")
-                    issueResolution = "".join(parseHtml.xpath('//*[@id="resolution-val"]/text()')).replace("\n",
-                                                                                                           "").replace(" ",
-                                                                                                                       "")
-                    issueAffectsVersions = "".join(parseHtml.xpath('//*[@id="versions-val"]/text()')).replace("\n",
-                                                                                                              "").replace(
-                        " ", "")
-                    if issueAffectsVersions == "":
-                        issueAffectsVersions = "".join(parseHtml.xpath('//*[@id="versions-field"]/span/text()')).replace(
-                            "\n", "").replace(" ", "")
-                    issueComponents = "".join(parseHtml.xpath('//*[@id="components-field"]/a/text()')).replace("\n",
-                                                                                                               "").replace(
-                        " ", "")
-                    issueFixVersions = "".join(parseHtml.xpath('//*[@id="fixfor-val"]/text()')).replace("\n", "").replace(
-                        " ", "")
-                    issueLabels = "".join(
-                        parseHtml.xpath('//*[@id="wrap-labels"]/div[contains(@class,"labels-wrap value")]/span/text()'))
+        # issue详情
+        issueType = "".join(parseHtml.xpath('//*[@id="type-val"]/text()')).replace("\n", "").replace(" ", "")
+        issueStatus = "".join(parseHtml.xpath('//*[@id="status-val"]/span/text()')).replace("\n", "").replace(
+            " ", "")
+        issuePriority = "".join(parseHtml.xpath('//*[@id="priority-val"]/text()')).replace("\n", "").replace(
+            " ", "")
+        issueResolution = "".join(parseHtml.xpath('//*[@id="resolution-val"]/text()')).replace("\n",
+                                                                                               "").replace(" ",
+                                                                                                           "")
+        issueAffectsVersions = "".join(parseHtml.xpath('//*[@id="versions-val"]/text()')).replace("\n",
+                                                                                                  "").replace(
+            " ", "")
+        if issueAffectsVersions == "":
+            issueAffectsVersions = "".join(parseHtml.xpath('//*[@id="versions-field"]/span/text()')).replace(
+                "\n", "").replace(" ", "")
+        issueComponents = "".join(parseHtml.xpath('//*[@id="components-field"]/a/text()')).replace("\n",
+                                                                                                   "").replace(
+            " ", "")
+        issueFixVersions = "".join(parseHtml.xpath('//*[@id="fixfor-val"]/text()')).replace("\n", "").replace(
+            " ", "")
+        issueLabels = "".join(
+            parseHtml.xpath('//*[@id="wrap-labels"]/div[contains(@class,"labels-wrap value")]/span/text()'))
 
-                    # issue描述
-                    issueDescription = "\n".join(parseHtml.xpath('//*[@id="description-val"]/div/text()')).replace("<br>",
-                                                                                                                   "").replace("\xa0", "")
-                    # issueDescription = ""
+        # issue描述
+        issueDescription = "\n".join(parseHtml.xpath('//*[@id="description-val"]/div/text()')).replace("<br>",
+                                                                                                       "").replace(
+            "\xa0", "")
+        # issueDescription = ""
 
-                    # issue人员
-                    issueAssignee = "".join(parseHtml.xpath('//*[@id="assignee-val"]/span/text()')).replace("\n",
-                                                                                                            "").replace(" ",
-                                                                                                                        "")
-                    issueReporter = "".join(parseHtml.xpath('//*[@id="reporter-val"]/span/text()')).replace("\n",
-                                                                                                            "").replace(" ",
-                                                                                                                        "")
+        # issue人员
+        issueAssignee = "".join(parseHtml.xpath('//*[@id="assignee-val"]/span/text()')).replace("\n",
+                                                                                                "").replace(" ",
+                                                                                                            "")
+        issueReporter = "".join(parseHtml.xpath('//*[@id="reporter-val"]/span/text()')).replace("\n",
+                                                                                                "").replace(" ",
+                                                                                                            "")
 
-                    # issue日期
-                    issueCreateTime = "".join(parseHtml.xpath('//*[@id="created-val"]/time/@datetime')).replace("\n",
-                                                                                                                "").replace(
-                        " ", "")
-                    issueData = {"URL": url, "标题": issueTitle, "类型": issueType, "状态": issueStatus, "优先级": issuePriority,
-                                 "解决结果": issueResolution, "影响版本": issueAffectsVersions, "修复的版本": issueFixVersions,
-                                 "模块": issueComponents, "标签": issueLabels, "描述": issueDescription, "经办人": issueAssignee,
-                                 "报告人": issueReporter, "创建时间": issueCreateTime}
+        # issue日期
+        issueCreateTime = "".join(parseHtml.xpath('//*[@id="created-val"]/time/@datetime')).replace("\n",
+                                                                                                    "").replace(
+            " ", "")
+        issueUpdateTime = "".join(parseHtml.xpath('//*[@id="updated-val"]/time/@datetime')).replace("\n",
+                                                                                                    "").replace(
+            " ", "")
+        issueData = {"URL": url, "issueTitle": issueTitle, "issueType": issueType, "issueStatus": issueStatus, "issuePriority": issuePriority,
+                     "issueResolution": issueResolution, "issueAffectsVersions": issueAffectsVersions, "issueFixVersions": issueFixVersions,
+                     "issueComponents": issueComponents, "issueLabels": issueLabels, "issueDescription": issueDescription, "issueAssignee": issueAssignee,
+                     "issueReporter": issueReporter, "issueCreateTime": issueCreateTime, "issueUpdateTime": issueUpdateTime}
 
-                    log.logger.info(json.dumps(issueData, ensure_ascii=False))
-                    csv_writer.writerow(issueData)
-                except Exception as e:
-                    log.logger.exception(str(e))
-                finally:
-                    time.sleep(1)
-
-
-if __name__ == '__main__':
-    log = Logger()
-    Cookie = "_ga=GA1.2.331566738.1611638722; Hm_lvt_7b79eb9425c1aa6c616a82083aee4a4d=1615722075,1617012806; JSESSIONID=A92C6181B0ED12CB260082D344D34305; atlassian.xsrf.token=BXV3-HF5O-JMT0-SJH5_b016ad37d4e8e6d19174953c9ca02b63a16a8711_lin"
-    jira = Jira()
-    jira.getIssueHtmlData()
+        log.logger.info(json.dumps(issueData, ensure_ascii=False))
+        return issueData
